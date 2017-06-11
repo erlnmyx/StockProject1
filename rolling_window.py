a4afd75ee646d7cac5b8867736bcd8c496dfa591
chen8684 <c.chen8684@gmail.com>
2017-06-12 03:37:51 +0800

    retrieve data from aigaogao

---
+++ rolling_window.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# for moving average
+def rolling_window(a, window):
+    """ Efficient rolling statistics with NumPy: This is applied to Picker._statistics() to calculate statistics
+        and Summary.threshold() to calcuate threshold to trigger event
+        Reference from:
+        http://www.rigtorp.se/2011/01/01/rolling-statistics-numpy.html
+    """
+    shape = a.shape[:-1] + (a.shape[-1] - window + 1, window)
+    strides = a.strides + (a.strides[-1],)
+    return np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides) 
